e30ec8ad10e7d81f17082d51ca7c01ff103f02df
ItsDin <imdindini88@gmail.com>
2016-03-08 22:25:43 +0800

    Add one expression successfully. TODO:scrollView

--- app/src/main/java/co/watermelonime/InputView/Emoji/Emoji.java
+++ app/src/main/java/co/watermelonime/InputView/Emoji/Emoji.java
@@ -1,5 +1,6 @@
 package co.watermelonime.InputView.Emoji;
 
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ScrollView;
 
@@ -11,12 +12,19 @@ import co.watermelonime.InputView.Chinese.Sentence.LanguageSelector;
 public class Emoji extends ViewGroup {
     public static ScrollView scrollView;
 
+    public static CandidateEmojiView candidateEmojiView;
     public Emoji() {
         super(C.mainService);
-        setBackgroundColor(Colour.DISABLED);
+        setBackgroundColor(Colour.CHARACTER);
 
-//        LanguageSelector.setInputLanguage(LanguageSelector.EMOJI);
-        addView(C.sentenceView);
+        scrollView = new ScrollView(C.mainService);
+        //scrollView.addView(candidateEmojiView);
+
+        Expressions.buildKeys();
+        for (View i : Expressions.keys)
+            addView(i);
+
+//        addView(C.sentenceView);
 //        addView(scrollView);
 //        addView(C.emojiKeyboard);
     }
@@ -33,16 +41,7 @@ public class Emoji extends ViewGroup {
     protected void onLayout(boolean changed, int l, int t, int r, int b) {
         l = 0;
         t = 0;
-        C.sentenceView.layout(l, t, l + Size.WSentenceView, t + Size.HSentenceView);
-        l += Size.WSentenceView;
-        switch (LanguageSelector.inputLanguage) {
-            case LanguageSelector.CHINESE:
-                scrollView.layout(l, t, l + Size.WCandidateView, t + Size.HCandidateView);
-                C.emojiKeyboard.layout(l, t + Size.HCandidateVisible, l + Size.WKeyboard, t + Size.HInputView);
-                break;
-            case LanguageSelector.EMOJI:
-                C.emoji.layout(l, t, l + Size.WCandidateView, t + Size.HCandidateView);
-                break;
-        }
+        //C.sentenceView.layout(l, t, l + Size.WSentenceView, t + Size.HSentenceView);
+        Expressions.keys[0].layout(l, t, l + Size.WSentenceView, t + Size.HSentenceView);
     }
 }
